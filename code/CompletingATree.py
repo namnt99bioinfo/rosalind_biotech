670d699a6a936ebb2f1415a5ef56c18e93b35b01
kcda.namthanh <namnt99private@gmail.com>
2024-07-30 21:05:24 +0700

    [DT-4662] create readFasta function

--- code/CompletingATree.py
+++ code/CompletingATree.py
@@ -1,29 +1,12 @@
-with open("../data/rosalind_tree.txt", "r") as f:
-    n = int(f.readline())
-    adjacency_list = [line.strip().split(" ") for line in f]
+def completeATree(data):
+    n = data[0][0]
+    edges = data[1:]
+    print(n - len(edges) - 1)
 
-subtrees = []
-nodes = set()
-for i, j in adjacency_list:
-    if i in nodes or j in nodes:
-        for st in subtrees:
-            if i in st or j in st:
-                subtrees[subtrees.index(st)].append(i)
-                subtrees[subtrees.index(st)].append(j)
-                nodes.add(i), nodes.add(j)
-    else:
-        subtrees.append([i, j])
-        nodes.add(i), nodes.add(j)
 
-l = len(subtrees)
-for i in range(l):
-    for j in range(l):
-        if i == j:
-            break
-        if len(set(subtrees[i] + subtrees[j])) < len(subtrees[i]) + len(subtrees[j]):
-            subtrees[i] = list(set(subtrees[i] + subtrees[j]))
-            subtrees[j] = []
-subtrees = [i for i in subtrees if i]
-
-result = (n - len(nodes)) + len(subtrees) - 1
-print(result)
+with open('../data/rosalind_tree.txt', 'r') as f:
+    data = []
+    for line in f:
+        splitData = [int(x) for x in line.split()]
+        data.append(splitData)
+    completeATree(data)
---
+++ code/CountingPhylogeneticAncestors.py
@@ -0,0 +1,6 @@
+
+
+
+with open('../data/rosalind_inod.txt', 'r') as f:
+    totalNodes = f.readline().strip()
+    print(int(totalNodes) - 2)
